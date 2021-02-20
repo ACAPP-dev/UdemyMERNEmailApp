94a6ba4392420e1f46e8e82af01fc30ef42e721d
ACAPP-dev <acapp909@gmail.com>
2021-02-20 14:43:47 -0800

    set up mongo db and user model

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+require("./models/user");
 require("./services/passport");
 
 const app = express();
@@ -14,3 +16,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+mongoose.connect(process.env.MONGO_URI);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,20 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       // function to add data to database
-      console.log("accessToken", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+      //console.log("accessToken", accessToken);
+      //console.log("refreshToken: ", refreshToken);
+      //console.log("profile: ", profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a user - future functionality
+          console.log(existingUser);
+        } else {
+          new User({
+            googleId: profile.id,
+            name: profile.displayName,
+          }).save();
+        }
+      });
     }
   )
 );
