b06113fc286c0cba8aaba6ed78d26c4bb1f78f52
ACAPP-dev <acapp909@gmail.com>
2021-02-20 11:20:11 -0800

    added google oath

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
+require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // function to add data to database
+      console.log("accessToken", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-const PORT = process.env.PORT || 8080;
+// Needs to be 8080 for Docker (or tell Docker 5000)
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
