16d43c4db0905e9ff1673378802a550f1d95b750
ACAPP-dev <acapp909@gmail.com>
2021-02-25 10:30:40 -0800

    some heroku prod changes

--- server/index.js
+++ server/index.js
@@ -26,6 +26,20 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
+// code for Express to recognize front end in Heroku production environment
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // such as main.js or main.css
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html file
+  // if it doesn't recognize the route.
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
